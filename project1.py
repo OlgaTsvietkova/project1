39662203af405a98e09dd31c1941b38dfcf33cd5
Olga Tsvietkova <flowerincitym@gmail.com>
2022-06-14 19:07:01 +0300

    Almost there

--- project1.py
+++ project1.py
@@ -1,56 +1,61 @@
-# board = ('-' * 20)
-# mark = input('Please write your mark ("o" or "x"): ')
-# position = int(input('Choose a number from 1 to 20: '))
-
 
 def evaluate(board):
-    board = ('-' * 20)
     if ('xxx' in board):
-        print('x') # The player who uses crosses (Xs) has won
+        result ='x' # The player who uses crosses (Xs) has won
     elif ('ooo' in board): 
-        print('o') # The player who uses noughts (Os) has won
+        result ='o' # The player who uses noughts (Os) has won
     elif ('-' not in board): 
-        print('!')  # Draw (the board is full but nobody has won)
+        result ='!'  # Draw (the board is full but nobody has won)
     else:
-        print('-') #Rest (i.e. the game is not finished)
+        result = '-' #Rest (i.e. the game is not finished)
+    return result
 
-def move(board, mark, position):
-    board = ('-' * 20)
-    # Returns the game board with the given mark in the given position.
-    return print(board[:position - 1] + mark + board[position + 1:])
+def move(board, mark, position): # Returns the game board with the given mark in the given position.
+    move_on_board = board[:position - 1] + mark + board[position + 1:]
+    return move_on_board
+
 
 def player_move(board, position):
-    board = ('-' * 20)
+    new_position = int(input('What position number do you choose?'))
     while True:
-        if position<1:
+        if new_position<1:
             print ('You cannot use a negative number')
-        elif position>20:
+            int(input('What position number do you choose?'))
+        elif new_position>20:
             print ('The number is too big')
-        elif '-' not in board[position]:
+            int(input('What position number do you choose?'))
+        elif '-' not in board[new_position]:
             print('The spot is taken, try again')
+            int(input('What position number do you choose?'))
         else:
-            return move(board, 'x', position)
+            board = move(board, 'x', new_position)
         break
+    return board
 
-def pc_move(board):
-    # Returns a game board with the computer's move.
-    from random import randrange
-    board = ('-' * 20)
-    position = randrange(19)
-    while '-' in board:
-        if '-' in board:
-            return move(board, 'o', position)
+from random import randrange
+from turtle import position
+def pc_move(board): # Returns a game board with the computer's move.
+    pc_position = randrange(19)
+    while True:
+        if board[pc_position] == "x":
+            pc_position = randrange (19)
+        elif board[pc_position] == "o":
+            pc_position = randrange (19)
+        elif board[pc_position] == "-":
+            board = move(board, 'o', pc_position)
         break
+    return board
 
-  
+
 def tictactoe():
     board = ('-' * 20)
     print(board)
     while '-' in board:
-        board = player_move(board, int(input('Choose a number from 1 to 20: ')))
+        board = player_move(board, position)
         print(board)
+        print(evaluate(board))
         board = pc_move(board)
         print(board)
-        evaluate(board)
+        print(evaluate(board))
     
-tictactoe()
+print(tictactoe())
