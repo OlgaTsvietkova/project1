46bfe2b5c45a0ebba9ebe22265616331b965093a
Olga Tsvietkova <flowerincitym@gmail.com>
2022-06-30 18:35:31 +0300

    Now works better but still has mistakes

--- project1.py
+++ project1.py
@@ -1,65 +1,71 @@
 def evaluate(board):
-    if 'xxx' in board:
-        result ='x' # The player who uses crosses (Xs) has won
-    elif 'ooo' in board: 
-        result ='o' # The player who uses noughts (Os) has won
+    if 'XXX' in board:
+        result ='X won'   
+    elif 'OOO' in board: 
+        result ='O won' 
     elif '-' not in board: 
-        result ='!'  # Draw (the board is full but nobody has won)
+        result ='Draw! The board is full'  
     else:
-        result = '-' #Rest (i.e. the game is not finished)
+        result = 'Please continue playing' 
     return result
 
 def move(board, mark, position): # Returns the game board with the given mark in the given position.
-    move_on_board = board[:position - 1] + mark + board[position + 1:]
+    move_on_board = board[:position - 1] + mark + board[position:]
     return move_on_board
 
 
-def player_move(board, position):
+def player_move(board):
     while True:
+        position = int(input('What position number do you choose? '))
         if position<1:
             print ('You cannot use a negative number')
-            int(input('What position number do you choose?'))
         elif position>20:
             print ('The number is too big')
-            int(input('What position number do you choose?'))
         elif '-' not in board[position]:
             print('The spot is taken, try again')
-            int(input('What position number do you choose?'))
         else:
-            board = move(board, 'x', position)
-        break
-    return board
+            board = move(board, 'X', position)
+            return board
 
 from random import randrange
 
-
 def pc_move(board): # Returns a game board with the computer's move.
-    pc_position = randrange(1,21)
     while True:
-        if board[pc_position] == "-":
-            board = move(board, 'o', pc_position)
-        else:
+        pc_position = randrange(1, 21)
+        if board[pc_position] == "X":
             pc_position = randrange(1,21)
-        break
-    return board
+        elif board[pc_position] == "O":
+            pc_position = randrange(1,21)
+        elif board[pc_position] == "-":
+            board = move(board, 'O', pc_position)
+        return board
 
 def tictactoe():
+    print('Hello player! This is a tic tac toe game where you play against computer')
+    print('Good luck!')
     board = ('-' * 20)
     print(board)
     while True:
-        board = player_move(board, int(input('What position number do you choose?')))
-        print(board)
+        board = player_move(board)
         result = evaluate(board)
-        if result== '-':
-            print(result)
+        if result == 'Please continue playing':
+            print('Game contiues')
+            print('Your move: ' + board)
+        elif result == 'X won':
+            print('Congrats, you won!')
+        elif result == 'O won':
+            print('Sorry, computer won')
         else:
+            print(board)
             break
+
         board = pc_move(board)
-        print(board)
         result = evaluate(board)
-        if result == '-':
-            print(result)
+        if result == 'Please continue playing':
+            print('Game contiues')
+            print('Computer move: ' + board)
         else:
+            print(board)
             break
     
-print(tictactoe())
+tictactoe()
